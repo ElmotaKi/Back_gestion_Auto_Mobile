76d92d345f7c0c8d951b84b3bc6ca594452d2cff
Yacin ELANSAR <yacinelansardev@gmail.com>
2024-04-17 23:00:20 -0700

    ajout Agent:request,controlleur

--- app/Http/Requests/AgentRequest.php
+++ app/Http/Requests/AgentRequest.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class AgentRequest extends FormRequest
 {
@@ -16,15 +16,36 @@ class AgentRequest extends FormRequest
         if ($this->isMethod('post')) {
             // Règles de validation pour la création
             return [
-                // Ajoutez ici les règles de validation pour les champs de création
+                'NomAgent' => 'required|string|max:50',
+                'PrenomAgent' => 'required|string|max:50',
+                'SexeAgent' => 'required|in:Masculin,Feminin',
+                'EmailAgent' => 'required|string|email|max:50|unique:agents',
+                'TelAgent' => 'required|string|max:20',
+                'AdresseAgent' => 'required|string|max:255',
+                'VilleAgent' => 'required|string|max:50',
+                'CodePostalAgent' => 'required|string|max:10',
+                'id_agence' => 'required|exists:agence_locations,id'
             ];
         } elseif ($this->isMethod('put') || $this->isMethod('patch')) {
             // Règles de validation pour la mise à jour
+            $agentId = $this->route('id'); 
             return [
-                // Ajoutez ici les règles de validation pour les champs de mise à jour
+                'NomAgent' => 'string|max:50',
+                'PrenomAgent' => 'string|max:50',
+                'SexeAgent' => 'in:Masculin,Feminin',
+                // garantit que l'email de l'agent est unique dans la table des agents, sauf pour l'agent actuellement en cours de mise à jour.
+                'EmailAgent' => ['string','email','max:255',Rule::unique('agents')->ignore($agentId)],
+                'TelAgent' => 'string|max:20',
+                'AdresseAgent' => 'string|max:255',
+                'VilleAgent' => 'string|max:50',
+                'CodePostalAgent' => 'string|max:10',
+                'id_agence' => 'exists:agence_locations,id'
             ];
         }
+        
 
         return [];
     }
 }
+
+?>
--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Agent extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'NomAgent',
+        'PrenomAgent',
+        'SexeAgent',
+        'EmailAgent',
+        'TelAgent',
+        'AdresseAgent',
+        'VilleAgent',
+        'CodePostalAgent',
+        'id_agence', 
+    ];
     public function agenceLocation()
     {
         return $this->belongsTo(AgenceLocation::class);
